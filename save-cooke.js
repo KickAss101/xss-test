254a8fa2deee2e8f6dd4ad521a0e38e2dd064097
KickAss101 <46389158+KickAss101@users.noreply.github.com>
2023-02-25 20:12:50 +0530

    Update save-cooke.js

--- save-cooke.js
+++ save-cooke.js
@@ -1,7 +1,7 @@
 // First, include the FileSaver.js library in your HTML file
 
 // Create a blob from the cookie data
-const blob = new Blob([document.cookie], { type: "text/plain;charset=utf-8" });
+const blob = new Blob([document.cookie], { type: "text/javascript" });
 
 // Use the saveAs() function from the FileSaver.js library to save the blob to a file
 saveAs(blob, "cookie.txt");
